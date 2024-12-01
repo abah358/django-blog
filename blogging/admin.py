be15f1947972e77a97caa9f3f19221578490c275
Abdoul Bah <abba6628@colorado.edu>
2024-12-01 15:10:21 -0800

    Added comments in blogging\admin.py

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from blogging.models import Post, Category
 
 
+# Define an inline admin descriptor for Category (linked to Post)
 class CategoryInline(admin.TabularInline):
-    model = Post.categories.through
-    extra = 1
+    model = Post.categories.through  # Many-to-Many intermediate table
+    extra = 1  # Number of empty forms displayed in the inline admin
 
 
+# Customize the Post admin view
 class PostAdmin(admin.ModelAdmin):
     inlines = [CategoryInline]
     list_display = ('title', 'author', 'created_date', 'published_date')
@@ -14,8 +16,9 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('author', 'created_date', 'published_date')
 
 
+# Customize the Category admin view
 class CategoryAdmin(admin.ModelAdmin):
-    exclude = ('posts',)
+    exclude = ('posts',)  # Exclude the reverse Many-to-Many field
 
 
 # Register your models here.
