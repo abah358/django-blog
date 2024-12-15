b83579e65f49c06ea0de6df0b4ec3b1c9abf0c45
Abdoul Bah <abba6628@colorado.edu>
2024-12-14 17:41:00 -0800

    Homework 8 successfully implemented

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,9 @@
 # from django.conf.urls import url
 from django.urls import path
-from blogging.views import stub_view, list_view, detail_view
+from blogging.views import BlogListView, BlogDetailView
 
-urlpatterns = [path('', list_view, name="blog_index"),
-               path('posts/<int:post_id>/', detail_view, name="blog_detail")]
+urlpatterns = [
+    path('', BlogListView.as_view(), name="blog_index"),
+    path('posts/<int:pk>/', BlogDetailView.as_view(), name="blog_detail")
+]
 
